3d99a1ea3c5540e0019757469e9a7ccc7243e377
Martín Lobo <kefala@MacBook-Pro-de-Martin.local>
2016-12-20 15:40:11 -0300

    Api domain

--- src/Core/runs.js
+++ src/Core/runs.js
@@ -1,8 +1,8 @@
 angular
     .module('almundo')
     .run(['$rootScope', function ($rootScope) {
-        if (location.host.indexOf("jabamastudio.com") != -1) {
-            $rootScope.apiHost = "//api-resto.jabamastudio.com";
+        if (location.host.indexOf("almundo.herokuapp") != -1) {
+            $rootScope.apiHost = "//almundo.herokuapp.com";
         }
         else  {
             $rootScope.apiHost = "//localhost:3000";
